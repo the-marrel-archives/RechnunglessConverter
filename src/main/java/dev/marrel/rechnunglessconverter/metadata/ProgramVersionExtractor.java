4614fec36716265c663772183c46a1509084f408
mxmarrel <mx@marrel.dev>
2025-01-05 12:28:59 +0100

    Added Mustang Version to ProgramVersionExtractor

--- src/main/java/dev/marrel/rechnunglessconverter/metadata/ProgramVersionExtractor.java
+++ src/main/java/dev/marrel/rechnunglessconverter/metadata/ProgramVersionExtractor.java
@@ -1,10 +1,11 @@
 package dev.marrel.rechnunglessconverter.metadata;
 
+import org.mustangproject.ZUGFeRD.Version;
 import org.mustangproject.ZUGFeRD.XRechnungImporter;
 
 public class ProgramVersionExtractor implements MetadataExtractor {
     @Override
     public String getValue(XRechnungImporter xrechung) {
-        return "RechnunglessConverter v" + ProgramVersionExtractor.class.getPackage().getImplementationVersion()/* + " w/ MustangProject v" + ZUGFeRDImporter.class.getPackage().getImplementationVersion()*/;
+        return "RechnunglessConverter v" + ProgramVersionExtractor.class.getPackage().getImplementationVersion() + " w/ MustangProject v" + Version.VERSION;
     }
 }
