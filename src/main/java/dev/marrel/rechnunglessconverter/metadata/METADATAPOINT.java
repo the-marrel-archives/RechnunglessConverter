5b92693dd7bd349fdfaed51326ea483ff3853d14
mxmarrel <mx@marrel.dev>
2025-01-05 12:29:21 +0100

    Added prefixes to MetadataPoints

--- src/main/java/dev/marrel/rechnunglessconverter/metadata/METADATAPOINT.java
+++ src/main/java/dev/marrel/rechnunglessconverter/metadata/METADATAPOINT.java
@@ -4,24 +4,29 @@ import org.mustangproject.ZUGFeRD.XRechnungImporter;
 import org.mustangproject.ZUGFeRD.ZUGFeRDImporter;
 
 public enum METADATAPOINT implements MetadataExtractor {
-    issueDate (ZUGFeRDImporter::getIssueDate),
-    dueDate (ZUGFeRDImporter::getDueDate),
-    totalAmount (ZUGFeRDImporter::getAmount),
-    buyerName (ZUGFeRDImporter::getBuyerTradePartyName),
-    paymentTerms (ZUGFeRDImporter::getPaymentTerms),
-    invoiceID (ZUGFeRDImporter::getInvoiceID),
-    sellerBIC (ZUGFeRDImporter::getBIC),
-    sellerBankName (ZUGFeRDImporter::getBankName),
-    sellerIBAN (ZUGFeRDImporter::getIBAN),
-    currency (ZUGFeRDImporter::getInvoiceCurrencyCode),
-    deliveryPeriod (new DeliveryPeriodExtractor()),
-    rechnunglessVersion(new ProgramVersionExtractor()),
+    issueDate ("invoice", ZUGFeRDImporter::getIssueDate),
+    dueDate ("invoice", ZUGFeRDImporter::getDueDate),
+    totalAmount ("invoice", ZUGFeRDImporter::getAmount),
+    buyerName ("invoice", ZUGFeRDImporter::getBuyerTradePartyName),
+    paymentTerms ("invoice", ZUGFeRDImporter::getPaymentTerms),
+    invoiceID ("invoice", ZUGFeRDImporter::getInvoiceID),
+    sellerBIC ("invoice", ZUGFeRDImporter::getBIC),
+    sellerBankName ("invoice", ZUGFeRDImporter::getBankName),
+    sellerIBAN ("invoice", ZUGFeRDImporter::getIBAN),
+    currency ("invoice", ZUGFeRDImporter::getInvoiceCurrencyCode),
+    deliveryPeriod ("invoice", new DeliveryPeriodExtractor()),
+    programVersion("rechnungless", new ProgramVersionExtractor()),
     //VALIDITY
     ;
 
     private final MetadataExtractor extractor;
+    public final String prefix;
     METADATAPOINT(MetadataExtractor extractor) {
+        this("", extractor);
+    }
+    METADATAPOINT(String prefix, MetadataExtractor extractor) {
         this.extractor = extractor;
+        this.prefix = prefix;
     }
 
     @Override
